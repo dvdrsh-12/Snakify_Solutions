2003b53f72a24be6844c02d9b248a974926c5858
Devadarsh M R <devadarshmr12567@gmail.com>
2024-02-23 22:14:11 +0530

    Two timestamps  added

---
+++ Input,print_and_numbers/Two_timestamps.py
@@ -0,0 +1,12 @@
+h1 = int(input())
+m1 = int(input())
+s1 = int(input())
+
+h2 = int(input())
+m2 = int(input())
+s2 = int(input())
+
+S1 = (h1*60*60)+(m1*60)+s1
+S2 = (h2*60*60)+(m2*60)+s2
+
+print(S2 - S1)
