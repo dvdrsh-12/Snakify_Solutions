45bd3be32b10baaab16841c343a6f7cdc2fc5aef
Devadarsh M R <devadarshmr12567@gmail.com>
2024-03-02 00:01:50 +0530

    The index of the maximum of a sequence added

--- While_loop/The_index_of_the_maximum_of_a_sequence.py
+++ While_loop/The_index_of_the_maximum_of_a_sequence.py
@@ -1,10 +1,14 @@
 maxnum = -1
+index = 0
+maxindex = 0
 
 while True:
     n = int(input())
     if n == 0:
         break
+    index = index + 1
     if n > maxnum:
         maxnum = n
+        maxindex = index
 
-print(maxnum)
+print(maxindex)
